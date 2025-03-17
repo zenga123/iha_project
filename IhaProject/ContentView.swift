2079071786c094974ceda20e54787ecccbd95157
zenga123 <zenga85@naver.com>
2025-03-17 22:30:11 +0900

    Initial Commit

---
+++ IhaProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IhaProject
+//
+//  Created by musung on 2025/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IhaProject/IhaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IhaProjectApp.swift
+//  IhaProject
+//
+//  Created by musung on 2025/03/17.
+//
+
+import SwiftUI
+
+@main
+struct IhaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
